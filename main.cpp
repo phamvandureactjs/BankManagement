37fcaa1dbe3f30837df301172933ae992c0b72f7
Pham Van Du <phamvandu.uet.vnu@gmail.com>
2024-01-06 17:55:08 +0700

    Haizz2

--- main.cpp
+++ main.cpp
@@ -2,20 +2,38 @@
 #include <string>
 #include <vector>
 #include <string.h>
+#include <chrono>
+#include <thread>
 using namespace std;
 struct InfoPersonal
 {
-    char * IDPersonal;
-    char * name;
-    char * dateOfBirth;
+    string IDPersonal;
+    string name;
+    string dateOfBirth;
 };
 struct AccountInfo
 {
-    char* numberAccount;
-    char* pinCode;
+    string numberAccount;
+    string pinCode;
     int balance;
 };
-
+void Loading(string s)
+{
+    cout << s;
+    cout.flush();
+    for (int j = 0; j < 20; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            cout << ".";
+            cout.flush();
+            // Sử dụng std::this_thread::sleep_for để thay thế _sleep
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+        cout << "\b\b\b   \b\b\b";
+    }
+    cout << " successful..." << endl;
+}
 std::string gen_random(const int len)
 {
     static const char alphanum[] = "0123456789";
@@ -29,6 +47,9 @@ std::string gen_random(const int len)
 
     return tmp_s;
 }
+class HandleJson
+{
+};
 
 class PersonalAccount
 {
@@ -39,66 +60,70 @@ private:
 public:
     PersonalAccount()
     {
-        infoPersonal.name = new char[100];
-        infoPersonal.IDPersonal = new char[100];
-        infoPersonal.dateOfBirth = new char[100];
-        accountInfo.numberAccount = new char[100];
-        accountInfo.pinCode = new char[100];
         accountInfo.balance = 0;
     }
-    // PersonalAccount(struct InfoPersonal _infoPersonal, struct AccountInfo _accountInfo)
+    // struct InfoPersonal getInfoPersonal()
     // {
-    //     infoPersonal.name = _infoPersonal.name;
-    //     accountInfo.balance = _accountInfo.balance;
+    //     return infoPersonal;
     // }
-    struct InfoPersonal getInfoPersonal()
+    // struct AccountInfo getAccountInfo()
+    // {
+    //     return accountInfo;
+    // }
+    string getName()
     {
-        return infoPersonal;
-    }
-    struct AccountInfo getAccountInfo()
-    {
-        return accountInfo;
-    }
-    char * getName(){
         return infoPersonal.name;
     }
-    void setName(char* _name){
+    void setName(string _name)
+    {
         infoPersonal.name = _name;
     }
-    char *getIDPersonal(){
+    string getIDPersonal()
+    {
         return infoPersonal.IDPersonal;
     }
-    void setIDPersonal(char *_IDPersonal){
+    void setIDPersonal(string _IDPersonal)
+    {
         infoPersonal.IDPersonal = _IDPersonal;
     }
-    char *getDOB(){
+    string getDOB()
+    {
         return infoPersonal.dateOfBirth;
     }
-    void setDOB(char *_DOB){
+    void setDOB(string _DOB)
+    {
         infoPersonal.dateOfBirth = _DOB;
     }
-    char * getPinCode(){
+    string getPinCode()
+    {
         return accountInfo.pinCode;
     }
-    void setPinCode(char *_pinCode){
+    void setPinCode(string _pinCode)
+    {
         accountInfo.pinCode = _pinCode;
     }
-    char * getNumAccount(){
+    string getNumAccount()
+    {
         return accountInfo.numberAccount;
     }
-    void setNumAccount(char *_numAccount){
+    void setNumAccount(string _numAccount)
+    {
         accountInfo.numberAccount = _numAccount;
     }
     int getBalance()
     {
         return accountInfo.balance;
     }
-    void setBalance(int _balance){
+    void setBalance(int _balance)
+    {
         accountInfo.balance = _balance;
     }
 };
 class BankManagement
 {
+private:
+    static std::vector<PersonalAccount> userInfor;
+
 public:
     BankManagement()
     {
@@ -107,58 +132,144 @@ public:
     static void CreateAccountBank(PersonalAccount &personalAccount)
     {
         cin.ignore();
+        string name;
         cout << "Type Name:";
-        fgets(personalAccount.getInfoPersonal().name, 20, stdin);
+        getline(cin, name);
+        personalAccount.setName(name);
+        string IDPersonal;
         cout << "Type ID Personal:";
-        fgets(personalAccount.getInfoPersonal().IDPersonal, 20, stdin);
+        getline(cin, IDPersonal);
+        personalAccount.setIDPersonal(IDPersonal);
+        string dob;
         cout << "Type Date Of Birth:";
-        fgets(personalAccount.getInfoPersonal().dateOfBirth, 20, stdin);
-        personalAccount.setBalance(0);
-        personalAccount.setPinCode((char *)(gen_random(6).c_str()));
-        personalAccount.setNumAccount((char *)(gen_random(12).c_str()));
+        getline(cin, dob);
+        personalAccount.setDOB(dob);
+        // personalAccount.setBalance(0);
+        personalAccount.setPinCode((gen_random(6)));
+        personalAccount.setNumAccount((gen_random(12)));
+        Loading("Creating account");
         userInfor.push_back(personalAccount);
-        // for(int i = 0; i < userInfor.size(); i++){
-        //     cout << userInfor[i].getName() << " " << userInfor[i].getIDPersonal() << " " << userInfor[i].getDOB() << " " << userInfor[i].getNumAccount() << " " << userInfor[i].getPinCode() << " " << userInfor[i].getBalance()<< endl;
-        // }
+        cout << personalAccount.getNumAccount() << endl;
+        ;
     }
-    static void Deposite()
+    static void Deposite(std::vector<PersonalAccount> &listAccount)
     {
         cout << "=============Welcome to my service=============" << endl;
-        cout << "Please to type the number you want to deposite to your account: ";
+        cout << "Please to type the number money you want to deposite to your account: ";
         int numDeposite;
         cin >> numDeposite;
         cin.ignore();
-        char *numAccount = new char[100];
-        cout << "Please type the number account you want to deposite: ";
-        fgets(numAccount, 3, stdin);
-        for(int i = 0; i < getVector().size(); i++){
-            if (strcmp(numAccount, getVector()[i].getIDPersonal()) == 0){
-                cout << "in if" << endl;
-                getVector()[i].setBalance(getVector()[i].getBalance() + numDeposite);
+        string numAccount;
+        cout << "Please type the your number account: ";
+        getline(cin, numAccount);
+        for (int i = 0; i < listAccount.size(); i++)
+        {
+            cout << listAccount[i].getNumAccount() << " ";
+            if (numAccount == listAccount[i].getNumAccount())
+            {
+                int nowMoney = listAccount[i].getBalance();
+                nowMoney += numDeposite;
+                listAccount[i].setBalance(nowMoney);
+                break;
             }
         }
-        // for(int i = 0; i < getVector().size(); i++){
-        //     cout << getVector()[i].getBalance() << " " << endl;
-        // }
-
+        setVector(listAccount);
     }
-    void Withdraw()
+    static void Withdraw(std::vector<PersonalAccount> &listAccount)
     {
+        cin.ignore();
+        string numAccount;
+        cout << "Please type your number account: ";
+        getline(cin, numAccount);
+        for (int i = 0; i < listAccount.size(); i++)
+        {
+            if (numAccount == listAccount[i].getNumAccount())
+            {
+                cout << "Please type the money you want to withdraw: ";
+                int numWithDraw;
+                cin >> numWithDraw;
+                while (numWithDraw > listAccount[i].getBalance())
+                {
+                    cout << "The balance is not enough. Please type money you want to withdraw again: ";
+                    cin >> numWithDraw;
+                }
+                int moneyRemain = listAccount[i].getBalance();
+                moneyRemain -= numWithDraw;
+                listAccount[i].setBalance(moneyRemain);
+                break;
+            }
+        }
+        setVector(listAccount);
+        Loading("Withdrawing");
     }
-    void changePINCode()
+    static void ChangePINCode(std::vector<PersonalAccount> &listAccount)
     {
+        cin.ignore();
+        string numAccount;
+        cout << "Please type your number account: ";
+        getline(cin, numAccount);
+        for (int i = 0; i < listAccount.size(); i++)
+        {
+            if (numAccount == listAccount[i].getNumAccount())
+            {
+                cout << "Please type the pincode you want to change: ";
+                string pinCode;
+                getline(cin, pinCode);
+                listAccount[i].setPinCode(pinCode);
+                break;
+            }
+        }
+        setVector(listAccount);
+        Loading("Changing");
     }
-    void Transfer()
+    static void Transfer(std::vector<PersonalAccount> &listAccount)
     {
+        cin.ignore();
+        string numAccount;
+        cout << "Please type your number account: ";
+        getline(cin, numAccount);
+        for (int i = 0; i < listAccount.size(); i++)
+        {
+            if (numAccount == listAccount[i].getNumAccount())
+            {
+                cout << "Please type the account you want to transfer: ";
+                string numAccountTransfer;
+                getline(cin, numAccountTransfer);
+                for (int j = 0; j < listAccount.size(); j++)
+                {
+                    if (numAccountTransfer == listAccount[j].getNumAccount())
+                    {
+                        cout << "Please type the money you want transfer: ";
+                        int moneyTransfer;
+                        cin >> moneyTransfer;
+                        while (moneyTransfer > listAccount[i].getBalance())
+                        {
+                            cout << "Sorry! Your balance is not enough to transfer!!!" << endl;
+                        }
+                        int newMoney = listAccount[j].getBalance();
+                        newMoney += moneyTransfer;
+                        listAccount[j].setBalance(newMoney);
+                        int oldMoney = listAccount[i].getBalance();
+                        oldMoney -= moneyTransfer;
+                        listAccount[i].setBalance(oldMoney);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        setVector(listAccount);
     }
-    static std::vector<PersonalAccount> getVector() {
+    static std::vector<PersonalAccount> getVector()
+    {
         return userInfor;
     }
-
-private:
-    static std::vector<PersonalAccount> userInfor;
+    static void setVector(std::vector<PersonalAccount> _vector)
+    {
+        userInfor = _vector;
+    }
 };
-std::vector<PersonalAccount> BankManagement::userInfor(0);
+std::vector<PersonalAccount> BankManagement::userInfor = {};
 int main()
 {
     cout << "===========Welcome to my bank===========" << endl;
@@ -172,8 +283,8 @@ int main()
         cout << "\t\033[33m3. \033[0mWithdraw" << endl;
         cout << "\t\033[33m4. \033[0mChange PIN code" << endl;
         cout << "\t\033[33m5. \033[0mTransfer" << endl;
-        cout << "\t\033[33m6. \033[0mTransfer cash to account" << endl;
-        cout << "\t\033[33m7. \033[0mExit" << endl;
+        // cout << "\t\033[33m6. \033[0mTransfer cash to account" << endl;
+        cout << "\t\033[33m6. \033[0mExit" << endl;
         cout << "\t============================" << endl;
         int choice;
         cout << endl;
@@ -187,27 +298,90 @@ int main()
             cout << "Thank you for believe our banking. We need some your personal information. Please type carefully!!!" << endl;
             PersonalAccount a;
             BankManagement::CreateAccountBank(a);
+            cout << "Do you want to process another function Y/N ?" << endl;
+            char kitu;
+            cin >> kitu;
+            if (kitu == 'Y')
+            {
+            }
+            else
+            {
+                cout << "=============Thank for using my service=============" << endl;
+                exit(0);
+            }
         }
         else if (choice == 2)
         {
-            BankManagement::Deposite();
+            vector<PersonalAccount> vector = BankManagement::getVector();
+            BankManagement::Deposite(vector);
+            cout << "Do you want to process another function Y/N ?" << endl;
+            char kitu;
+            cin >> kitu;
+            if (kitu == 'Y')
+            {
+            }
+            else
+            {
+                cout << "=============Thank for using my service=============" << endl;
+                exit(0);
+            }
         }
         else if (choice == 3)
         {
+            vector<PersonalAccount> vector = BankManagement::getVector();
+            BankManagement::Withdraw(vector);
+            cout << "Do you want to process another function Y/N ?" << endl;
+            char kitu;
+            cin >> kitu;
+            if (kitu == 'Y')
+            {
+            }
+            else
+            {
+                cout << "=============Thank for using my service=============" << endl;
+                exit(0);
+            }
         }
         else if (choice == 4)
         {
+            vector<PersonalAccount> vector = BankManagement::getVector();
+            BankManagement::ChangePINCode(vector);
+            cout << "Do you want to process another function Y/N ?" << endl;
+            char kitu;
+            cin >> kitu;
+            if (kitu == 'Y')
+            {
+            }
+            else
+            {
+                cout << "=============Thank for using my service=============" << endl;
+                exit(0);
+            }
         }
         else if (choice == 5)
         {
+            vector<PersonalAccount> vector = BankManagement::getVector();
+            BankManagement::Transfer(vector);
+            cout << "Do you want to process another function Y/N ?" << endl;
+            char kitu;
+            cin >> kitu;
+            if (kitu == 'Y')
+            {
+            }
+            else
+            {
+                cout << "=============Thank for using my service=============" << endl;
+                exit(0);
+            }
         }
-        else if (choice == 6)
-        {
-        }
-        else if (choice == 7)
-        {
-        }
-        else if (choice == 8)
+        // else if (choice == 6)
+        // {
+        //     // for (int i = 0; i < BankManagement::getVector().size(); i++)
+        //     // {
+        //     //     cout << BankManagement::getVector()[i].getBalance() << " " << endl;
+        //     // }
+        // }
+        else
         {
         }
     }
